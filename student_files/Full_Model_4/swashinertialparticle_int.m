150b3d5ac8686994071a3cd572adf4bf8f95b65a
Benjamin Davidson <136382571+davidson-ben@users.noreply.github.com>
2024-12-09 15:47:17 -0600

    Delete student_files directory

--- student_files/Full_Model_4/swashinertialparticle_int.m
+++
@@ -1,9 +0,0 @@
-function [t,xp] = swashinertialparticle_int(s,St,H,xp0,t,mu,Cm,gamma,k,shift)
-%swashinertialparticle_int.m Set up integration of swashinertialparticle_ode.m
-
-% function to integrate
-rhs = @(t,xp) swashinertialparticle_ode(s,St,H,xp,t,mu,Cm,gamma,k,shift);
-% integrate
-[t,xp] = ode45(rhs,t,xp0); 
-
-end
